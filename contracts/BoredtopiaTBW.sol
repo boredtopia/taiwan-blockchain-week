695ae36a03ca1fe632f8ede307c4d7dca8d0996f
diewland <diewland@gmail.com>
2024-12-07 11:37:07 +0700

    add contract

---
+++ contracts/BoredtopiaTBW.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract BoredtopiaTBW is ERC721A, Ownable {
+
+    // config
+    constructor(address initialOwner)
+        ERC721A("Boredtopia Taiwan Blockchain Week", "BOREDTOPIATBW")
+        Ownable(initialOwner) {
+    }
+    uint256 public MAX_MINT_PER_WALLET = 3;
+    uint256 public START_ID = 1;
+
+    bool public mintEnabled = false;
+    string public baseURI = "https://boredtopia.github.io/taiwan-blockchain-week/json/";
+
+    // start token id
+    function _startTokenId() internal view virtual override returns (uint256) {
+        return START_ID;
+    }
+
+    // metadata
+    function setBaseURI(string calldata _newBaseURI) external onlyOwner {
+        baseURI = _newBaseURI;
+    }
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        return string.concat(baseURI, Strings.toString(tokenId), ".json");
+    }
+
+    // toggle sale
+    function toggleSale() external onlyOwner {
+        mintEnabled = !mintEnabled;
+    }
+
+    // mint
+    function mint(uint quantity, bytes32[] calldata _merkleProof) external {
+        require(mintEnabled, "Sale is not enabled");
+        require(_numberMinted(msg.sender) + quantity <= MAX_MINT_PER_WALLET, "Over wallet limit");
+        
+        _mint(msg.sender, quantity);
+    }
+    function adminMint(uint quantity) external onlyOwner {
+        _mint(msg.sender, quantity);
+    }
+
+    // aliases
+    function numberMinted(address owner) external view returns (uint256) {
+        return _numberMinted(owner);
+    }
+    function remainingSupply() external pure returns (uint256) {
+        return 0;
+    }
+
+}
